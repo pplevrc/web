7f421c6c87bb28c708d511dee50d906528a22d1f
mewton <capella@mail.mewton.jp>
2024-11-22 23:08:59 +0900

    fix: レスポンシブの計算が狂っていた問題の対応

--- src/components/layouts/ResponsiveScript/scripts.ts
+++ src/components/layouts/ResponsiveScript/scripts.ts
@@ -25,21 +25,9 @@ export function calculateRootFontSize({
 	defaultRootFontSize = getRootFontSize(),
 	viewportWidth = getViewportWidth(),
 }: CalculateFontSizeOptions = {}): number {
-	if (isSP()) {
-		// smartphone viewport
-		// vw 1440px .. 32px
-		// vw 412px .. 16px
-		// となる式
-		// return (viewportWidth / (PC_VIEWPORT_WIDTH - 412) + 2 - 412 / (PC_VIEWPORT_WIDTH - 412) * defaultRootFontSize;
-		return (viewportWidth / 1024 + 0.5992217899) * defaultRootFontSize;
-	}
-
-	// pc viewport
-	// vw 1440px .. 16px
-	// vw 2880px .. 32px
-	// となる式
-
-	return (viewportWidth / PC_VIEWPORT_WIDTH) * defaultRootFontSize;
+	return (
+		(viewportWidth / PC_VIEWPORT_WIDTH) * defaultRootFontSize * (isSP() ? 3 : 1)
+	);
 }
 
 export function setRootFontSize(fontSize: number): void {
