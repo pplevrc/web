cfe5d3bd5c5f85814a118b6f43d6da85bcfb3ee7
mew-ton <mxl_usmarc@slmail.mewton.jp>
2025-04-13 17:19:02 +0900

    feat: 店員さん一覧をある程度作った

--- src/components/domains/avatar/DecorativeAvatar/type.ts
+++ src/components/domains/avatar/DecorativeAvatar/type.ts
@@ -2,5 +2,5 @@ import type { HTMLAttributes } from "astro/types";
 import type { Props } from "../images/AvatarImage.astro";
 
 export interface CommonProps extends Omit<Props, "alt"> {
-  rootAttributes: HTMLAttributes<"figure">;
+  rootAttributes?: HTMLAttributes<"figure">;
 }
--- src/components/layouts/Sections/HomePickupSection/type.ts
+++
@@ -1,4 +0,0 @@
-/**
- * ※ 必要最低限のみ型を実装する
- */
-export type ThemeColor = "rose" | "matcha";
