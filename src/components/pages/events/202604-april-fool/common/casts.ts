2f64f38864f003e45740d1b118d77d74d4a6ab9e
mew-ton <mxl_usmarc@slmail.mewton.jp>
2026-03-27 23:37:05 +0900

    fix: 一部のアセットを反映

--- src/components/pages/events/202604-april-fool/common/casts.ts
+++ src/components/pages/events/202604-april-fool/common/casts.ts
@@ -1,20 +1,55 @@
 import { getEntry } from "astro:content";
+import ImageHiraxu from "@assets/images/events/202604-april-fool/chara-hiraxu.png";
+import ImageStora from "@assets/images/events/202604-april-fool/chara-stora.png";
+
 import type { Cast } from "@content/casts";
 
-const APRIL_FOOL_MAIN_CAST_IDS: string[] = [
+/**
+ * エイプリルフール限定キャスト用データセット
+ */
+export interface AprilFoolEventMainCast {
+  id: Cast["vrchat"]["userId"];
+
+  nickname: Cast["profile"]["nickname"];
+
+  image: ImageMetadata;
+
+  themeColor: Cast["themeColor"];
+
+  introduction: Cast["profile"]["introduction"];
+}
+
+const APRIL_FOOL_MAIN_CAST_IDS = [
   "桃小姫ストラ",
   "くるりおうか",
   "ひらくぅ。",
   "sao*",
-];
+] as const satisfies AprilFoolEventMainCast["id"][];
 
-export async function loadCasts(): Promise<Cast[]> {
+type MainCastId = (typeof APRIL_FOOL_MAIN_CAST_IDS)[number];
+
+const IMAGE_MAP = {
+  桃小姫ストラ: ImageStora,
+  // TODO: まだイラストできてない. でき次第差し替える
+  くるりおうか: ImageStora,
+  "ひらくぅ。": ImageHiraxu,
+  // TODO: まだイラストできてない. でき次第差し替える
+  "sao*": ImageStora,
+} as const satisfies Record<MainCastId, ImageMetadata>;
+
+export async function loadCasts(): Promise<AprilFoolEventMainCast[]> {
   const casts = [];
 
   for (const id of APRIL_FOOL_MAIN_CAST_IDS) {
     const { data: cast } = (await getEntry("casts", id)) ?? { data: undefined };
     if (cast) {
-      casts.push(cast);
+      casts.push({
+        id: cast.vrchat.userId,
+        nickname: cast.profile.nickname,
+        image: IMAGE_MAP[id],
+        themeColor: cast.themeColor,
+        introduction: cast.profile.introduction,
+      });
     }
   }
 
